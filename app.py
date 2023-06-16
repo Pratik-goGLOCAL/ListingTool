8f40873f4ce14793a56a8fcc0b1f24c8953352d6
Pratik Ghongade <pratik.g@goglocal.com>
2023-06-16 15:18:43 +0530

    updated with new Api

--- app.py
+++ app.py
@@ -41,8 +41,6 @@ if "Region" not in st.session_state:
     st.session_state["Region"] = ""
 if "Market_Place" not in st.session_state:
     st.session_state["Market_Place"] = ""
-if "asin_brand" not in st.session_state:
-    st.session_state["asin_brand"] = ""
 if "ASIN" not in st.session_state:
     st.session_state["ASIN"] = ""
 if "category" not in st.session_state:
@@ -72,49 +70,35 @@ marketplace = marketplace_place.multiselect(label='Select Market Places',
 # st.write('The options selected are:', marketplace)
 st.session_state['Market_Place'] = marketplace
 
-asin_brand_place = st.empty()
+# asin_brand_place = st.empty()
 
-asin_brand = asin_brand_place.selectbox(label='Select ASIN or Brand',
-                    options=['Brand Name','ASIN'],disabled=False)
-st.session_state['asin_brand'] = asin_brand
+# asin_brand = asin_brand_place.selectbox(label='Select ASIN or Brand',
+#                     options=['Brand Name','ASIN'],disabled=False)
+# st.session_state['asin_brand'] = asin_brand
 
 category_place = st.empty()
 category = category_place.selectbox(label='Select the Category',
                     options=list(amazon_categories.keys()) ,disabled=False)
 # st.write('The options selected are:', marketplace)
 st.session_state['category'] = category
-logger.info(st.session_state['asin_brand'])
+# logger.info(st.session_state['asin_brand'])
 # Select the Brand Name
-if st.session_state['asin_brand']=='ASIN':
-    asin_place = st.empty()
-    search_text = asin_place.text_input("Search for an ASIN", st.session_state["ASIN"])
-    # logger.info([x.strip() for x in search_text.split(',')])
-    
-    st.session_state["ASIN"] = search_text #[x.strip() for x in search_text.split(',')]
-    with open('DataStore/All_Asins.pickle','wb') as handle:
-            pickle.dump(search_text.split(','), handle, protocol=pickle.HIGHEST_PROTOCOL)
-elif st.session_state['asin_brand']=='URL':
-    url_place = st.empty()
-    search_text = url_place.text_input("Search for an URL", st.session_state["URL"])
-    # logger.info([x.strip() for x in search_text.split(',')])
-    
-    st.session_state["URL"] = search_text #[x.strip() for x in search_text.split(',')]
-else:
-    brandname_place = st.empty()
-    search_text = brandname_place.text_input("Search for a Brand Name (if multiple then separate using ' , ') e.g. Yellow Chimes", st.session_state["Brand_name"])
-    
-    st.session_state["Brand_name"] =search_text #[x.strip() for x in search_text.split(',')]
+# if st.session_state['asin_brand']=='ASIN':
+asin_place = st.empty()
+asins = asin_place.text_input("Search for an ASIN", st.session_state["ASIN"])
+# logger.info([x.strip() for x in search_text.split(',')])
+
+st.session_state["ASIN"] = asins if len(asins)>0 else 'NA'#[x.strip() for x in search_text.split(',')]
+
+brandname_place = st.empty()
+brand = brandname_place.text_input("Search for a Brand Name (if multiple then separate using ' , ') e.g. Yellow Chimes", st.session_state["Brand_name"])
+
+st.session_state["Brand_name"] =brand #[x.strip() for x in search_text.split(',')]
 submitplace = st.empty()
 submit = submitplace.button("Submit",disabled=False,key='submit1')
 stop = st.button('Stop')
 db = st.empty()
 # pd.DataFrame([brand_name],columns=['keyword_list']).to_csv('DataStore/keyword_list.csv',index=False)
-keyword_list = {st.session_state['asin_brand']:search_text}
-logger.info(keyword_list)
-with open('DataStore/keyword_list.pickle', 'wb') as handle:
-    pickle.dump(keyword_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
-with open('DataStore/_category.pickle', 'wb') as handle:
-    pickle.dump(amazon_categories[category], handle, protocol=pickle.HIGHEST_PROTOCOL)
 @st.cache
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
@@ -130,53 +114,58 @@ if not stop and submit:
                     options=available_marketplaces,
                     default = ['Amazon'],disabled = True,key = 'marketplace')
     
-    if st.session_state['asin_brand']=='ASIN':
-        asin_place.text_input("Search for an ASIN", st.session_state["ASIN"],disabled = True,key = 'asinplace')
-    else:
-        brandname_place.text_input("Search for a Brand Name (if multiple then separate using ' , ') e.g. Yellow Chimes", st.session_state["Brand_name"],disabled = True,key = 'brandplace')
-    asin_brand_place.selectbox(label='Select ASIN or Brand',
-                    options=['Brand Name','ASIN'],disabled=True, key = 'asin_brand')
+    asin_place.text_input("Search for an ASIN", st.session_state["ASIN"],disabled = True,key = 'asinplace')
+    brandname_place.text_input("Search for a Brand Name (if multiple then separate using ' , ') e.g. Yellow Chimes", st.session_state["Brand_name"],disabled = True,key = 'brandplace')
     category_place.selectbox(label='Select the Category',
                     options=list(amazon_categories.keys()) ,disabled=True,key = 'category')
     st.caption('The Scraping+Listing QC Checks are in process. PLEASE DO NOT CLOSE THE TAB')
     st.caption('To stop the process press the Stop button and refresh the page')
-    textplace = st.empty()
-    textplace.subheader("Scraping Started for {} ".format(search_text))
-    ##################################################################################################################################
-    # import subprocess
-    # variable = 'Run_Spider.py'
-    # subprocess.call(f"{sys.executable} " + variable, shell=True)
-    # try:
-    #     df = pd.read_csv('DataStore/Scrapy_Res.csv')
-    #     df.fillna('NA',inplace = True)  
-    #     # df['product_brand'] = df['product_brand'].apply(lambda x:st.session_state['Brand_name'].title() if x=='NA' else x).copy()
-    # except:
-    #     df = pd.read_csv('DataStore/ScrapedData_pg_v1.csv')
-    #     df.fillna('NA',inplace = True)
-    # # listing_cols = ['ASIN', 'MRP', 'aplus_images', 'aplus_text', 'brand', 'bullets', 'description', 'image_links',
-    # #                 'price', 'ratings', 'ratings_count', 'title', 'url', 'video_links']
-    # df = df#[listing_cols]
-    ##################################################################################################################################
-    logger.info('asin_brand:{}, search_text:{}, category:{}'.format(st.session_state['asin_brand'],search_text,st.session_state['category']))
-    response = requests.get(f"{'http://52.204.224.231:8027/Scrapping?brand_asin={}&search_text={}&category={}'.format(st.session_state['asin_brand'],search_text,st.session_state['category'])}")
-    logger.info('We got the Response form API')
-    dict = json.loads(response.json())
-    logger.info('Reponse JSON dict {}'.format(dict))
-    df = pd.DataFrame.from_dict(dict)
-    logger.info('DataFrame Head is {}'.format(df.head()))
-    ##################################################################################################################################
-    textplace.subheader('Scraping Complete!!!')
-    # st.write(df)
-    minpdata = 2
-    timetorun = str(round(len(df)*minpdata/60,1))+' hours' if (len(df)*minpdata)/60 >1 else str(len(df)*minpdata)+' minutes'
-    st.caption('Estimated time for completion is about {} '.format(timetorun))
-    # st.write(os.listdir('DataStore/'))
-    st.subheader('QC_Checks Started')
-    df.fillna('NA',inplace = True)
-    res_df = excel_checks.QC_check1(df)
-    # res_df = df.copy()
-    # res_df = res_df.drop(['product_weight','product_material','product_category','item_height','item_length','item_width'], axis = 1)
-    st.subheader('QC Checks Completed!!!')
+    r_email = 'NA' if len(r_email)>0 else r_email
+    query = {'brand':st.session_state['Brand_name'],
+             'asin_list': st.session_state['ASIN'],
+             'category':st.session_state['category'],
+             'r_email':r_email}
+    qc_response = requests.post(f"http://52.204.224.231:8031/ListingQC",json=query)
+    if r_email!="NA":
+        st.caption('The results are successfully sent on you email address !!')
+    res_df = pd.read_json(qc_response.json(), orient='records')
+    # textplace = st.empty()
+    # textplace.subheader("Scraping Started for {} ".format(search_text))
+    # ##################################################################################################################################
+    # # import subprocess
+    # # variable = 'Run_Spider.py'
+    # # subprocess.call(f"{sys.executable} " + variable, shell=True)
+    # # try:
+    # #     df = pd.read_csv('DataStore/Scrapy_Res.csv')
+    # #     df.fillna('NA',inplace = True)  
+    # #     # df['product_brand'] = df['product_brand'].apply(lambda x:st.session_state['Brand_name'].title() if x=='NA' else x).copy()
+    # # except:
+    # #     df = pd.read_csv('DataStore/ScrapedData_pg_v1.csv')
+    # #     df.fillna('NA',inplace = True)
+    # # # listing_cols = ['ASIN', 'MRP', 'aplus_images', 'aplus_text', 'brand', 'bullets', 'description', 'image_links',
+    # # #                 'price', 'ratings', 'ratings_count', 'title', 'url', 'video_links']
+    # # df = df#[listing_cols]
+    # ##################################################################################################################################
+    # logger.info('asin_brand:{}, search_text:{}, category:{}'.format(st.session_state['asin_brand'],search_text,st.session_state['category']))
+    # response = requests.get(f"{'http://52.204.224.231:8027/Scrapping?brand_asin={}&search_text={}&category={}'.format(st.session_state['asin_brand'],search_text,st.session_state['category'])}")
+    # logger.info('We got the Response form API')
+    # dict = json.loads(response.json())
+    # logger.info('Reponse JSON dict {}'.format(dict))
+    # df = pd.DataFrame.from_dict(dict)
+    # logger.info('DataFrame Head is {}'.format(df.head()))
+    # ##################################################################################################################################
+    # textplace.subheader('Scraping Complete!!!')
+    # # st.write(df)
+    # minpdata = 2
+    # timetorun = str(round(len(df)*minpdata/60,1))+' hours' if (len(df)*minpdata)/60 >1 else str(len(df)*minpdata)+' minutes'
+    # st.caption('Estimated time for completion is about {} '.format(timetorun))
+    # # st.write(os.listdir('DataStore/'))
+    # st.subheader('QC_Checks Started')
+    # df.fillna('NA',inplace = True)
+    # res_df = excel_checks.QC_check1(df)
+    # # res_df = df.copy()
+    # # res_df = res_df.drop(['product_weight','product_material','product_category','item_height','item_length','item_width'], axis = 1)
+    # st.subheader('QC Checks Completed!!!')
     submitplace.button("Submit",disabled=False,key='submit3')
     email_place.text_input('Enter e-mail address to get results via mail', st.session_state["r_email"],disabled=False,key = 'emailplace3')
     region_place.multiselect(label='Select Region',
@@ -185,21 +174,17 @@ if not stop and submit:
     marketplace_place.multiselect(label='Select Market Places',
                     options=available_marketplaces,
                     default = ['Amazon'],disabled = False,key = 'marketplace3')
-    if st.session_state['asin_brand']=='ASIN':
-        asin_place.text_input("Search for an ASIN", st.session_state["ASIN"],disabled = False,key = 'asinplace3')
-    else:
-        brandname_place.text_input("Search for a Brand Name (if multiple then separate using ' , ') e.g. Yellow Chimes", st.session_state["Brand_name"],disabled = False,key = 'brandplace3')
-    asin_brand_place.selectbox(label='Select ASIN or Brand',
-                    options=['Brand Name','ASIN'],disabled=True, key = 'asin_brand3')
+    asin_place.text_input("Search for an ASIN", st.session_state["ASIN"],disabled = False,key = 'asinplace3')
+    brandname_place.text_input("Search for a Brand Name (if multiple then separate using ' , ') e.g. Yellow Chimes", st.session_state["Brand_name"],disabled = False,key = 'brandplace3')
     category_place.selectbox(label='Select the Category',
                     options=list(amazon_categories.keys()) ,disabled=True,key = 'category3')
     # st.dataframe(res_df[[col for col in res_df.columns if 'Corrected_text' not in col]])
-    st.dataframe(res_df)
+    # st.dataframe(res_df)
 
-    now = datetime.now()
-    dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
-    filename = 'Listing_QC_results_'+dt_string+'.csv'
-    res_df.to_csv('DataStore/'+filename ,index = False)
+    # now = datetime.now()
+    # dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
+    # filename = 'Listing_QC_results_'+dt_string+'.csv'
+    # res_df.to_csv('DataStore/'+filename ,index = False)
     
 
     # if 'ScrapedData_pg_v1.csv' in os.listdir('DataStore/'):
@@ -217,8 +202,8 @@ if not stop and submit:
     db.download_button(
         label="Download",
         data=csv,
-        file_name='DataStore/ListingQC_res.csv',
+        file_name='ListingQC_res.csv',
         mime='text/csv',
     )
-    if len(r_email)>0:
-        send_email(r_email,filename)
+    # if len(r_email)>0:
+    #     send_email(r_email,filename)
